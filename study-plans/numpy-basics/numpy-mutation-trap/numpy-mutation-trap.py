2d82f96ae2ad682f2b626558b693e14dcbe11a95
FWD-LeTung <lethanhtung9319@gmail.com>
2026-07-05 12:34:18 +0700

    Solve numpy-mutation-trap

--- study-plans/numpy-basics/numpy-mutation-trap/numpy-mutation-trap.py
+++ study-plans/numpy-basics/numpy-mutation-trap/numpy-mutation-trap.py
@@ -5,4 +5,4 @@ def original_and_clipped(data, row_idx, lo, hi):
     Returns: 2D ndarray of float64 with shape (2, ncols)
     """
     x = np.array(data, dtype=np.float64)
-    return np.stack((x[row_idx],np.clip(x[row_idx], lo, hi)))
+    return np.stack((x[row_idx].copy(),np.clip(x[row_idx], lo, hi)))
