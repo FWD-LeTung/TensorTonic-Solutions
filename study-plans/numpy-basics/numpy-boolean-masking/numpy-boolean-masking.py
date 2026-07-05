03671d9d7e15b76d300301b831150d9499512990
FWD-LeTung <lethanhtung9319@gmail.com>
2026-07-05 11:58:52 +0700

    Solve numpy-boolean-masking

--- study-plans/numpy-basics/numpy-boolean-masking/numpy-boolean-masking.py
+++ study-plans/numpy-basics/numpy-boolean-masking/numpy-boolean-masking.py
@@ -3,20 +3,11 @@ import numpy as np
 def row_summary(data, threshold):
     """Returns: np.ndarray of shape (3, m, n), stacked element mask, any-filtered, all-filtered"""
     x = np.array(data, dtype=np.float64)
-    y1 = np.where(x>threshold, 1., 0.)
+    y1 = (x>threshold).astype(np.float64)
     cond1 = np.any(x>threshold, axis=1)
     cond2 = np.all(x>threshold, axis=1)
     y2 = np.empty_like(x)
     y3 = np.empty_like(x)
-    for i, value in enumerate(cond1):
-        if value:
-            y2[i] = x[i].copy()
-        else:
-            y2[i] = np.zeros(x[i].shape)
-    
-    for i, value in enumerate(cond2):
-        if value:
-            y3[i] = x[i].copy()
-        else:
-            y3[i] = np.zeros(x[i].shape)
+    y2 = np.where(cond1[:,np.newaxis], x, 0.0)
+    y3 = np.where(cond2[:,np.newaxis], x, 0.0)
     return np.stack([y1, y2, y3], axis=0)
